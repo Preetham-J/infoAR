8a7fa30250332f62f56fa262b776895458780bfc
Eric Tran <erictraaaan@gmail.com>
2018-01-28 00:56:26 -0500

    added assets

---
+++ ARtest/Assets/myScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myScript : MonoBehaviour {
+
+	public GameObject sign;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	float currentVal = 0;
+
+	bool isOn = true;
+		
+	// Update is called once per frame
+	void Update () {
+//		currentVal = Input.GetAxis ("Horizontal");
+		currentVal = Input.touchCount;
+	
+//		print (Input.GetAxis ("Horizontal"));
+		if (currentVal != 0){
+			sign.SetActive(!isOn);
+
+			isOn = !isOn;
+
+			DestroyObject (sign);
+//			sign.GetComponent<Renderer> ().enabled = false;
+		}
+
+
+	}
+}
